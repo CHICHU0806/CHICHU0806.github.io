1cf959a3f919d9386b4a714df0dbc8eff28720b1
CHICHU <2085219596@qq.com>
2026-06-07 21:04:16 +0800

    6.7 新增卡尔曼滤波（KF）的基础理论推导

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,12 +1,22 @@
 export const posts = [
     {
-        id: '1',
+        id: '3-1',
         title: '何为四元数&Mahony空间四元数解算',
         date: '2026-06-06',
         category: '数学理论推导系列教程',
         tags: ['四元数', 'Mahony解算'],
-        summary: '这是一篇用于测试 Markdown 与 KaTeX 公式渲染的文章。',
+        summary: '本文对四元数的定义、性质以及在空间解算中的应用进行详细介绍，并通过Mahony解算方法展示其实际应用。',
         file: '何为四元数&Mahony空间四元数解算',
+        featured: true,
+    },
+    {
+        id: '3-2',
+        title: '卡尔曼滤波（KF）的基础理论推导',
+        date: '2026-06-06',
+        category: '数学理论推导系列教程',
+        tags: ['卡尔曼滤波', 'KF'],
+        summary: '本文对卡尔曼滤波从一阶向二阶在特定环境下进行了初步的推导，并且为后面向EKF的拓展埋下伏笔。',
+        file: '卡尔曼滤波（KF）的基础理论推导',
         featured: true,
     },
 ]
