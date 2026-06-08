a5ef3135c4cba3454bfa84470fc31b5f3b0b8c62
CHICHU <2085219596@qq.com>
2026-06-08 18:44:15 +0800

    6.8 更新标题栏

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/CHICHU.github.io/',
+    base: '/',
     build: {
         outDir: 'docs',
         emptyOutDir: true
