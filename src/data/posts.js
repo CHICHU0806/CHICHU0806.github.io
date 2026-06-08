44c5b05d30c059555dae11b0eea62b0fa3196066
CHICHU <2085219596@qq.com>
2026-06-09 00:39:03 +0800

    6.9 测试文件栏

--- src/data/posts.js
+++ src/data/posts.js
@@ -19,6 +19,36 @@ export const posts = [
         file: '卡尔曼滤波（KF）的基础理论推导',
         featured: true,
     },
+    {
+        id: '3-2',
+        title: '卡尔曼滤波（KF）的基础理论推导',
+        date: '2026-06-07',
+        category: '数学理论推导系列教程',
+        tags: ['卡尔曼滤波', 'KF'],
+        summary: '本文对卡尔曼滤波从一阶向二阶在特定环境下进行了初步的推导，并且为后面向EKF的拓展埋下伏笔。',
+        file: '卡尔曼滤波（KF）的基础理论推导',
+        featured: true,
+    },
+    {
+        id: '3-2',
+        title: '卡尔曼滤波（KF）的基础理论推导',
+        date: '2026-06-07',
+        category: '数学理论推导系列教程',
+        tags: ['卡尔曼滤波', 'KF'],
+        summary: '本文对卡尔曼滤波从一阶向二阶在特定环境下进行了初步的推导，并且为后面向EKF的拓展埋下伏笔。',
+        file: '卡尔曼滤波（KF）的基础理论推导',
+        featured: true,
+    },
+    {
+        id: '3-2',
+        title: '卡尔曼滤波（KF）的基础理论推导',
+        date: '2026-06-07',
+        category: '数学理论推导系列教程',
+        tags: ['卡尔曼滤波', 'KF'],
+        summary: '本文对卡尔曼滤波从一阶向二阶在特定环境下进行了初步的推导，并且为后面向EKF的拓展埋下伏笔。',
+        file: '卡尔曼滤波（KF）的基础理论推导',
+        featured: true,
+    },
 ]
 
 export function getPostById(id) {
